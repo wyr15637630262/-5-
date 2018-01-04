5cbb96602ba91e50e15c011cb902035158a1f125
wyr16537630262 <wyr15637630262@163.com>
2018-01-04 17:48:49 +0800

    first commit

---
+++ src/module/account/accountList.vue
@@ -0,0 +1,35 @@
+<!--
+  - accountList Vue 文件
+  - @JsName accountList
+  - @Description
+  - @DateTime 2018/1/4 17:33
+  - @author 亚茹
+  -->
+<template>
+  <div>
+    <m-Breadcrumb mTitle1="账号管理" mTitle2="账号列表"></m-Breadcrumb>
+    <div class="accountList_content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import mBreadcrumb from './../../component/headerTag.vue'
+  export default {
+    name: 'accountList',
+    data () {
+      return {
+      }
+    },
+    methods: {
+    },
+    mounted () {
+    },
+    components: {
+      mBreadcrumb
+    }
+  }
+</script>
+<!-- 引入 accountList Scss 文件  -->
+<style lang="scss" scoped>@import "scss/accountList";</style>
---
+++ src/module/system/paramConfig.vue
@@ -0,0 +1,34 @@
+<!--
+  - paramConfig Vue 文件
+  - @JsName paramConfig
+  - @Description
+  - @DateTime 2018/1/4 17:42
+  - @author 亚茹
+  -->
+<template>
+  <div>
+    <m-Breadcrumb mTitle1="系统管理" mTitle2="参数配置"></m-Breadcrumb>
+    <div class="paramConfig_content">
+
+    </div>
+  </div>
+</template>
+<script>
+  import mBreadcrumb from './../../component/headerTag.vue'
+  export default {
+    name: 'paramConfig',
+    data () {
+      return {
+      }
+    },
+    methods: {
+    },
+    mounted () {
+    },
+    components: {
+      mBreadcrumb
+    }
+  }
+</script>
+<!-- 引入 paramConfig Scss 文件  -->
+<style lang="scss" scoped>@import "scss/paramConfig";</style>
